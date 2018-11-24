a30e6bf84249ae57f9bb2546bd3592fa0d5a8b0d
keyhan jk <keyhan.jannat.khah@gmail.com>
2018-11-25 02:44:50 +0330

    -Wall

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
+struct s1 {
+    char *fname;
+
+};
+
 int main(int argc, char const *argv[])
 {
     printf("hello\n");
-
+ 
     int i = 5;
     float f = 5.6f;
-    i = f;
-    printf("%f\n", f);
+    i = f; 
     printf("%d\n", i);
-    scanf("%d", &i);  
+    printf("%f\n", f);
     return 0;
 }
